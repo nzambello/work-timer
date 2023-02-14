13017299cfda27089be79fe00913ea5b5f34e1c5
nzambello <nzambello@protonmail.com>
2023-02-14 10:13:40 +0100

    chore: setup app entries with mantine

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,19 +1,22 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react';
+import { startTransition, StrictMode } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { ClientProvider } from '@mantine/remix';
 
 function hydrate() {
   startTransition(() => {
     hydrateRoot(
       document,
       <StrictMode>
-        <RemixBrowser />
+        <ClientProvider>
+          <RemixBrowser />
+        </ClientProvider>
       </StrictMode>
     );
   });
 }
 
-if (typeof requestIdleCallback === "function") {
+if (typeof requestIdleCallback === 'function') {
   requestIdleCallback(hydrate);
 } else {
   // Safari doesn't support requestIdleCallback
