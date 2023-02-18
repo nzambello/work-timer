fec650d212785e7cdd158a7a44ed128d41f5c86a
nzambello <nzambello@protonmail.com>
2023-02-18 20:38:20 +0100

    chore: cleanup log

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -42,7 +42,6 @@ export async function verifyLogin(
       password: true
     }
   });
-  console.log(userWithPassword);
 
   if (!userWithPassword || !userWithPassword.password) {
     return null;
