3abe667e65ad36ed7ba1fc87d9d5550b2e944714
nzambello <nzambello@protonmail.com>
2023-03-01 01:21:23 +0100

    chore: cleanup defaults

--- prisma/migrations/20230301000042_add_userpref_dateformat/migration.sql
+++ prisma/migrations/20230301000042_add_userpref_dateformat/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "dateFormat" TEXT NOT NULL DEFAULT 'en-GB';
+ALTER TABLE "User" ADD COLUMN     "dateFormat" TEXT NOT NULL DEFAULT 'en-US';
---
+++ prisma/migrations/20230301002109_userpref_dateformat_default/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "dateFormat" SET DEFAULT 'en-GB';
